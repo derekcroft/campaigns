652d061742f7dc1122d811b71018cf3fc8e1f74a
Derek Croft <derek@derekcroft.com>
2012-02-09 17:50:20 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KiindlyCom::Application.config.secret_token = '843aaf667e5d833aaf7b258b92a5d8f011cabc60ef2f4dbb189435c43623587d200593a2445c0e092ea44e228be4d1b22be2c2f32d357fc413d2ccbef6ea20d6'
