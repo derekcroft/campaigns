d306684545379ffbf21070f2bc36e79ed6576af2
Derek Croft <derek@derekcroft.com>
2012-02-23 11:50:55 -0500

    Undo last change

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,6 +1,6 @@
 class Campaign < ActiveRecord::Base
   def percent_complete
     return 0 if donation_total.nil?
-    [(BigDecimal.new(donation_total)/BigDecimal.new(donation_target)*100).round.to_i, 100].min
+    [(donation_total/donation_target*100).round.to_i, 100].min
   end
 end
