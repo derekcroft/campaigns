d537e393bfd2b3c137db5fd073726c157e180624
Derek Croft <derek@derekcroft.com>
2012-03-10 13:23:44 -0500

    Redirect to static site unless the subdomain is dev

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :filter_subdomain
+  
+  def filter_subdomain
+    unless request.subdomain == 'dev'
+      redirect_to 'http://www.kiindly.net'
+      return false
+    end
+  end
 end
