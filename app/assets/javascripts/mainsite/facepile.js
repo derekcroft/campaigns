0893ad4e02505cd503a54246703de2b232fade4c
Derek Croft <derek@derekcroft.com>
2012-03-13 22:41:35 -0400

    Fix app ID that was pointing back to wrong app

--- app/assets/javascripts/mainsite/facepile.js
+++ app/assets/javascripts/mainsite/facepile.js
@@ -3,7 +3,7 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=276707242348301";
+  js.src = "//connect.facebook.net/en_GB/all.js#xfbml=1&appId=255484937866032";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 // End of facepile code
