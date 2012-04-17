641ea58c04d8374c44468c09b2c26c54dc76b5cd
Derek Croft <derek@derekcroft.com>
2012-04-17 15:05:30 -0400

    Validate presence of first and last names

--- app/models/donor.rb
+++ app/models/donor.rb
@@ -5,7 +5,7 @@ class Donor < ActiveRecord::Base
 
   attr_accessible :terms_of_service, :campaign_id, :first_name, :last_name, :email, :street_address, :city, :state, :zip, :phone, :email_opt_in
 
-  validates :campaign, :street_address, :city, :state, :zip, presence: true
+  validates :campaign, :first_name, :last_name, :street_address, :city, :state, :zip, presence: true
   validates :email, format: { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }, presence: true
   validates :stripe_customer, presence: true
   validates :terms_of_service, inclusion: [true]
