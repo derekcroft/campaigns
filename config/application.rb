07ccc85b7cf0067a181a46fd15d2350f9a2d4c26
Derek Croft <derek@derekcroft.com>
2012-04-19 10:33:26 -0400

    Add initialize_on_precompile setting

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module KiindlyCom
     # parameters by using an attr_accessible or attr_protected declaration.
     # config.active_record.whitelist_attributes = true
 
+    # fix precompile issue on Heroku
+    config.assets.initialize_on_precompile = false
+
     # Enable the asset pipeline
     config.assets.enabled = true
 
