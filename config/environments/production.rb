aed24ef9d3530483d9009636d5df3368a4189197
Derek Croft <derek@derekcroft.com>
2012-04-22 17:23:14 -0400

    Update Gmail settings for production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,18 @@ KiindlyCom::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'kiindly.com',
+    :user_name            => 'donation@kiindly.com',
+    :password             => 'd0n0rzz!',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
+
 end
