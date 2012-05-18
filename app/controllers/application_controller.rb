60cdb3bd7290fc63559d492b1684a4aafcbf9591
Derek Croft <derek@derekcroft.com>
2012-05-18 13:14:54 -0400

    Remove subdomain restriction from live site.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :filter_subdomain
   before_filter :load_campaign
-
-  def filter_subdomain
-    if Rails.env.production?
-      unless %w{dev oupledge}.include?(request.subdomain)
-        redirect_to 'http://www.kiindly.net'
-        return false
-      end
-    end
-  end
 
   def load_campaign
     @campaign = Campaign.first
