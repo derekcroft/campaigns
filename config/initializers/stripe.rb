550ebd17cd5b37b55cbe2136aadf5fbb2c6ff4a7
Derek Croft <derek@derekcroft.com>
2012-05-21 23:16:41 -0400

    Switch to live Stripe keys.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,2 +1,7 @@
-Stripe.api_key = "3vAWJJUrN1pyhnUOLvVCNehhrAEJ5gkS"
-STRIPE_PUBLISHABLE_KEY = "pk_3hEW6oAUjy89JDRHdYgeRwnSRfguG"
+# test
+#Stripe.api_key = "3vAWJJUrN1pyhnUOLvVCNehhrAEJ5gkS"
+#STRIPE_PUBLISHABLE_KEY = "pk_3hEW6oAUjy89JDRHdYgeRwnSRfguG"
+
+# live
+Stripe.api_key = "wb0cLIk2REiQaBg5FvLA8ltD1Hrk3vLY"
+STRIPE_PUBLISHABLE_KEY = "pk_va9FTtsmhIlv0cUIdcnaXsCMfIhr9"
