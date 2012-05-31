19da8b5fa8ba9aefba771caf3ce631eb9ebfb205
Derek Croft <derek@derekcroft.com>
2012-05-31 14:53:40 -0400

    Change Stripe keys to OUAA live keys.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,7 +1,17 @@
+## Kiindly.com Stripe keys
 # test
 #Stripe.api_key = "3vAWJJUrN1pyhnUOLvVCNehhrAEJ5gkS"
 #STRIPE_PUBLISHABLE_KEY = "pk_3hEW6oAUjy89JDRHdYgeRwnSRfguG"
 
 # live
-Stripe.api_key = "wb0cLIk2REiQaBg5FvLA8ltD1Hrk3vLY"
-STRIPE_PUBLISHABLE_KEY = "pk_va9FTtsmhIlv0cUIdcnaXsCMfIhr9"
+#Stripe.api_key = "wb0cLIk2REiQaBg5FvLA8ltD1Hrk3vLY"
+#STRIPE_PUBLISHABLE_KEY = "pk_va9FTtsmhIlv0cUIdcnaXsCMfIhr9"
+
+## OUAA Stripe keys
+# test
+#Stripe.api_key = "zdMlARuWbM3wUf8XLw37nVruppHdAaT9"
+#STRIPE_PUBLISHABLE_KEY = "pk_3nMQqDSMnDAUB8PcYGP2PVTCSjrI5"
+
+# live
+Stripe.api_key = "elub5pMJiQpkTknxOK50HCiSs3opsBDz"
+STRIPE_PUBLISHABLE_KEY = "pk_goE4UBY5zWbD39T98Y9vrfTJqdvN4"
