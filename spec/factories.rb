7576fd88bf1e350b424dab42064095540949912a
Derek Croft <derek@derekcroft.com>
2012-06-06 17:46:56 -0400

    Add fields to donor factory that were breaking specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,11 +5,15 @@ FactoryGirl.define do
 
   factory :donor do
     campaign
+    first_name 'James'
+    last_name 'Berger'
     email 'bob@emailville.com'
     street_address '2344 Willow Lane'
     city 'Bogliville'
     state 'IA'
     zip '75757'
+    stripe_customer 'customer_info_yaml'
+    terms_of_service true
   end
 
   factory :pledge do
