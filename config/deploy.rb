75d5f16fd502a6fbae57f3ceb0c4475fa687cf51
Derek Croft <derek@derekcroft.com>
2012-08-15 12:26:47 -0400

    Add bundle, change other settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,17 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "chi"
+set :repository,  "git@github.com:derekcroft/kiindly.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :use_sudo, false
+set :deploy_to, "/home/kiindly/campaigns/chi"
+
+server "kiindly", :app, :web, :db, primary: true
+#role :web, "kiindly"                          # Your HTTP server, Apache/etc
+#role :app, "kiindly"                          # This may be the same as your `Web` server
+#role :db,  "kiindly", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
@@ -16,10 +20,10 @@ role :db,  "your slave db-server here"
 # these http://github.com/rails/irs_process_scripts
 
 # If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :deploy do
+ task :start do ; end
+ task :stop do ; end
+ task :restart, :roles => :app, :except => { :no_release => true } do
+   run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+ end
+end
