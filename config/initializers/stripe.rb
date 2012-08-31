01eb58c05c6ba21f7d57170cc7448721093568f7
Derek Croft <derek@derekcroft.com>
2012-08-30 23:35:59 -0400

    Add Stripe keys for CHI.

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -13,5 +13,15 @@
 #STRIPE_PUBLISHABLE_KEY = "pk_3nMQqDSMnDAUB8PcYGP2PVTCSjrI5"
 
 # live
-Stripe.api_key = "elub5pMJiQpkTknxOK50HCiSs3opsBDz"
-STRIPE_PUBLISHABLE_KEY = "pk_goE4UBY5zWbD39T98Y9vrfTJqdvN4"
+#Stripe.api_key = "elub5pMJiQpkTknxOK50HCiSs3opsBDz"
+#STRIPE_PUBLISHABLE_KEY = "pk_goE4UBY5zWbD39T98Y9vrfTJqdvN4"
+
+## Healthy Chicago Initiative Stripe keys
+# test
+Stripe.api_key = 'sk_0EEhFAMHd3BuQH29KJevoALr9zld1'
+STRIPE_PUBLISHABLE_KEY = 'pk_0EEh2ZN9GM4wWLGEAEznbIysBQ4te'
+
+# live
+#Stripe.api_key = 'sk_0EEhccEW3E48nB1fklAaKFBnGNKdL'
+#STRIPE_PUBLISHABLE_KEY = 'pk_0EEhPDdlHd0gfqe8Yy1h2RhzSgmGw'
+
