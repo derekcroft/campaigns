92d99cd7c52d4ef0e3c605b5d4a9eb3a7eea5594
Derek Croft <derek@derekcroft.com>
2012-09-10 21:35:35 -0400

    Add caching code to campaigns_controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -2,12 +2,20 @@ class CampaignsController < ApplicationController
   force_ssl except: :index
 
   def index
-    show
-    render action: 'show'
+    last_pledge = @campaign.pledges.last
+    @pledge = @campaign.pledges.build
+    if stale?(last_pledge, public: true)
+      #show
+      render action: :show
+    end
+    expires_in 15.minutes, public: true
   end
 
   def show
+    last_pledge = @campaign.pledges.last
     @pledge = @campaign.pledges.build
+    fresh_when last_pledge, public: true
+    expires_in 15.minutes, public: true
   end
 
   def popup
