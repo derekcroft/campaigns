4edaeba7e92febd00619db13eb297db1d90a264e
Derek Croft <derek@derekcroft.com>
2012-10-29 17:00:11 -0400

    Move creation of campaigns to the seeds where it should be.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,15 @@ Donor.delete_all
 Campaign.delete_all
 Pledge.delete_all
 
-c = Campaign.create(donation_target: 50000)
+Campaign.create(
+  donation_target: 2500,
+  ends_on: Date.new(2012, 10, 31),
+  name: 'Healthy Chicago Initiative',
+  subdomain: 'chi',
+  contact_name: 'Fred Bear',
+  contact_email: 'freddy@kiindly.com',
+  starts_on: Date.new(2012, 9, 30),
+  contact_phone: '3133342344'
+)
+
 
