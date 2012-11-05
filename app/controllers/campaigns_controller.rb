478da59be885041dc33658a04b78a668cde2bdcf
Derek Croft <derek@derekcroft.com>
2012-11-05 11:14:30 -0500

    Remove show action and force_ssl from campaigns controller

--- app/controllers/campaigns_controller.rb
+++ app/controllers/campaigns_controller.rb
@@ -1,19 +1,10 @@
 class CampaignsController < ApplicationController
-  force_ssl except: [:index, :popup]
 
   def index
     last_pledge = @campaign.pledges.last
-    @pledge = @campaign.pledges.build
     if stale?(last_pledge, public: true)
       render action: :show
     end
-    expires_in 15.minutes, public: true
-  end
-
-  def show
-    last_pledge = @campaign.pledges.last
-    @pledge = @campaign.pledges.build
-    fresh_when last_pledge, public: true
     expires_in 15.minutes, public: true
   end
 
