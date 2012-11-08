17c6564d19ce0e96e6f08b6391a13e85afbf8826
Derek Croft <derek@derekcroft.com>
2012-11-08 11:53:29 -0500

    Update to project's API key with Airbrake

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,4 +1,4 @@
 Airbrake.configure do |config|
 #  config.api_key = '931fbcb8bd52addd10ed8bb55c46f648' #kiindlycom (heroku)
-  config.api_key = '1509a6165292dece1b5904e018df1bceed496fec'
+  config.api_key = '01364b637c25979b418aa28404acfb44'
 end
