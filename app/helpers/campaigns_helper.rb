d4af3f3e3b9c3940df27888970f2fa195a6519c6
Derek Croft <derek@derekcroft.com>
2012-11-13 14:43:50 -0500

    Link to the https version of the new pledges page from the landing page.

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -7,4 +7,8 @@ module CampaignsHelper
     campaign.starts_on.strftime('%b %-d, %Y')
   end
 
+  def count_me_in_button(host)
+    protocol = Rails.env.production? ? 'https' : 'http'
+    button_to "COUNT ME IN", { protocol: protocol, host: host, controller: 'pledges', action: 'new' }, { method: :get, style: 'width:230px;' }
+  end
 end
