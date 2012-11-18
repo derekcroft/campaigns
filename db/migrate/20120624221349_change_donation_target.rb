90c04a98ddb0aebb5b4340b924348276fbbca833
Derek Croft <derek@derekcroft.com>
2012-11-18 16:52:17 -0500

    Remove misbehaving migration

--- db/migrate/20120624221349_change_donation_target.rb
+++
@@ -1,9 +0,0 @@
-class ChangeDonationTarget < ActiveRecord::Migration
-  def up
-    Campaign.last.update_attribute(:donation_target, BigDecimal.new("2500"))
-  end
-
-  def down
-    Campaign.last.update_attribute(:donation_target, BigDecimal.new("50000"))
-  end
-end
