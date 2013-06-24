09326aa52748e38e3dbe365f73103ef62983a830
Derek Croft <derek@derekcroft.com>
2013-06-24 11:35:58 -0400

    Add @team variable to pledges#create so the share buttons have the full URL even when affiliate link was not chosen.

--- app/controllers/pledges_controller.rb
+++ app/controllers/pledges_controller.rb
@@ -12,6 +12,7 @@ class PledgesController < ApplicationController
     @pledge = @campaign.pledges.build(params[:pledge])
     @pledge.dot_color ||= Pledge.random_hex
     @pledge.pledge_type = 'dollar'
+    @team = @pledge.team
     begin
       api_key = AccessToken.stripe_api_key(@campaign.subdomain, @stripe_mode_param)
       c = Stripe::Customer.create({description: @pledge.donor.email, card: params[:stripe_card_token]}, api_key)
