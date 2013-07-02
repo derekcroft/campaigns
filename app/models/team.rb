8f5603b2d12a381f18ffb2e8c328b830c00691c3
Derek Croft <derek@derekcroft.com>
2013-07-02 17:25:34 -0400

    Add lts_leaderboard_query.sql.  Split leaderboard table into its own partial so it can be rendered for every sport.

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,49 +3,57 @@ require 'csv'
 class Team < ActiveRecord::Base
   attr_accessible :captain_email, :captain_first_name, :captain_last_name, :charity, :name, :program, :sourced_on, :sport, :url
 
-  def self.import!
-    CSV.foreach(File.join(Rails.root, 'db', "lts_teams.csv"), headers: :first_row) do |row|
-      Team.where(name: row['Team']).first_or_create(
-        captain_first_name: row['First Name'],
-        captain_last_name: row['Last Name'],
-        captain_email: row['Email'],
-        name: row['Team'],
-        program: row['Program'],
-        sport: row['Sport'],
-        charity: row['What do YOU play for?!'],
-        sourced_on: row['Source File Date'],
-        url: row['Team URL']
-      )
+  has_many :pledges
+
+  class << self
+    def import!
+      CSV.foreach(File.join(Rails.root, 'db', "lts_teams.csv"), headers: :first_row) do |row|
+        Team.where(name: row['Team']).first_or_create(
+          captain_first_name: row['First Name'],
+          captain_last_name: row['Last Name'],
+          captain_email: row['Email'],
+          name: row['Team'],
+          program: row['Program'],
+          sport: row['Sport'],
+          charity: row['What do YOU play for?!'],
+          sourced_on: row['Source File Date'],
+          url: row['Team URL']
+        )
+      end
     end
-  end
 
-  def self.teams_and_charities
-    Team.order(:name).all.collect {|t| ["#{t.name} / #{t.charity}", t.id]}
-  end
-
-  def self.teams_and_charities_all
-    Team.order(:name).uniq.collect do |team|
-      {
-        description: "#{team.name} / #{team.charity}",
-        team_id: team.id
-      }
+    def teams_and_charities
+      order(:name).all.collect {|t| ["#{t.name} / #{t.charity}", t.id]}
     end
-  end
 
-  def self.teams_and_charities_by_sport
-    Team.order(:name).uniq.inject({}) do |accum, team|
-      teams_for_sport = accum.fetch(team.sport, [])
-      teams_for_sport << {
-        description: "#{team.name} / #{team.charity}",
-        team_id: team.id
-      }
-      accum.store(team.sport, teams_for_sport)
-      accum
+    def teams_and_charities_all
+      order(:name).uniq.collect do |team|
+        {
+          description: "#{team.name} / #{team.charity}",
+          team_id: team.id
+        }
+      end
     end
-  end
 
-  def self.sports
-    Team.select(:sport).order(:sport).uniq.collect(&:sport)
+    def teams_and_charities_by_sport
+      order(:name).uniq.inject({}) do |accum, team|
+        teams_for_sport = accum.fetch(team.sport, [])
+        teams_for_sport << {
+          description: "#{team.name} / #{team.charity}",
+          team_id: team.id
+        }
+        accum.store(team.sport, teams_for_sport)
+        accum
+      end
+    end
+
+    def sports
+      select(:sport).order(:sport).uniq.collect(&:sport)
+    end
+
+    def leaders
+      select("name, charity, sport, count(pledges.id) as num_donors, sum(pledges.cap) as amount_raised").joins(:pledges).group('name, charity, sport').order('num_donors desc')
+    end
   end
 
   def subdomain
---
+++ db/lts_leaderboard_query.sql
@@ -0,0 +1,10 @@
+﻿select t.name, t.charity, t.sport,
+count(p.id) num_donors,
+sum(p.cap) amount_raised
+from teams t
+inner join pledges p
+on t.id = p.team_id
+--where t.sport = 'Kickball'
+group by t.name, t.charity, t.sport
+order by num_donors desc
+limit 10;
