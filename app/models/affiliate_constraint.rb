bc714c86dbb1289e2892a05e25659616e5a809de
Derek Croft <derek@derekcroft.com>
2013-07-12 13:56:48 -0400

    Create campaign subfolders in db directory to clean it up.

--- app/models/affiliate_constraint.rb
+++ app/models/affiliate_constraint.rb
@@ -1,9 +1,12 @@
 class AffiliateConstraint
   def initialize
     @subdomains = Team.all.collect(&:subdomain).uniq
+  rescue PG::Error
+    @cloud_66 = true
   end
 
   def matches?(request)
+    return true if @cloud_66
     @subdomains.include? request.fullpath.sub(/^\//, "")
   end
 end
