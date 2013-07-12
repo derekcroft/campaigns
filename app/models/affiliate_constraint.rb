f0dac7bb7765341de6001eb6363506768215383c
Derek Croft <derek@derekcroft.com>
2013-07-12 13:59:08 -0400

    Revert changes to AffiliateConstraint for Cloud66.

--- app/models/affiliate_constraint.rb
+++ app/models/affiliate_constraint.rb
@@ -1,12 +1,9 @@
 class AffiliateConstraint
   def initialize
     @subdomains = Team.all.collect(&:subdomain).uniq
-  rescue PG::Error
-    @cloud_66 = true
   end
 
   def matches?(request)
-    return true if @cloud_66
     @subdomains.include? request.fullpath.sub(/^\//, "")
   end
 end
