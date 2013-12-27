ab8f35dd8558a5ad8fae4132aa3e6f141a24a0cd
Derek Croft <derek@derekcroft.com>
2013-12-27 14:56:58 -0500

    Move LTS logo to MSUDM temporarily.  Add *.jpg to precompile list.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,6 +47,7 @@ KiindlyCom::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   config.assets.precompile += %w{
+    *.jpg
     campaigns.js
     campaigns.css
     pledges.js
